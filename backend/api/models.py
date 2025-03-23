1c802acb2131684ab8498bc3e0cb0102bd71171b
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-03-23 14:04:20 +0300

    base for models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    '''
+    Модель пользователя на основе импортируемой абстрактной модели.
+    '''
+    pass
+
+
+class Recipe(models.Model):
+    '''
+    Модель для рецептов.
+    '''
+    pass
+
+
+class Tag(models.Model):
+    '''
+    Модель для тэгов
+    '''
+    pass
+
+
+class Ingredient(models.Model):
+    '''
+    Модель для ингредиентов.
+    '''
+    pass
