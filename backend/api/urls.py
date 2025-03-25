efb4b0dacaf6a9fe4db4e1950e01e0410842d7c7
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-03-25 12:37:57 +0300

    urls register (router_v1)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,16 @@
 from django.urls import include, path
+from rest_framework import routers
 
+from . import views
 
 app_name = 'api'
+
+router_v1 = routers.DefaultRouter()
+
+router_v1.register('recipes', views.RecipeViewSet, basename='recipes')
+router_v1.register('tags', views.TagViewSet, basename='tags')
+router_v1.register('ingredients', views.IngredientViewSet, basename='ingredients')
+
+urlpatterns = [
+    path('', include(router_v1.urls))
+]
