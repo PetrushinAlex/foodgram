67b1bba06bc52cb5ffca4ac2cca73aba52a28ec8
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-03-25 12:22:06 +0300

    views base

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from . import models, serializers
+
+
+class RecipeViewSet(viewsets.GenericViewSet):
+    '''
+    Получение списка рецептов.
+    '''
+    queryset = models.Recipe.objects.all()
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    '''
+    Получение списка тэгов.
+    Создание и редактирование только в админке.
+    '''
+    queryset = models.Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    '''
+    Получение списка ингредиентов.
+    Создание и редактирование только в админке.
+    '''
+    queryset = models.Ingredient.objects.all()
