c678545e4a48c6140c446fd8f20876ed56c120db
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-03-28 15:21:35 +0300

    add model for tag

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,7 +12,24 @@ class Tag(models.Model):
     '''
     Модель для тэгов
     '''
-    pass
+
+    name = models.CharField(
+        max_length=250,
+        db_index=True,
+        verbose_name='Тэг',
+    )
+    color = models.CharField(
+        max_length=10,
+        verbose_name='слаг',
+    )
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
