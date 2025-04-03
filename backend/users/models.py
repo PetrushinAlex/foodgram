5d2d39ec556be063c7aefaedeb06fb5906dac7e5
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-03 15:49:54 +0300

    update customuser model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from tools import constants as cnst
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     '''
     Модель пользователя на основе импортируемой абстрактной модели.
     '''
-    pass
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=cnst.MAX_LENGHT_NAME,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=cnst.MAX_LENGHT_NAME,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=cnst.MAX_LENGHT_NAME,
+        verbose_name='Фамилия',
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
