596fc4bebb0e28c85681fcd5fa88ad2d65d46c17
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-04 17:20:26 +0300

    register customuser model in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    '''
+    Кастомный класс для регистрации модели пользователя в админке.
+    '''
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = (
+        'username',
+        'email',
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
