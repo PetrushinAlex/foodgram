fde8c3c6a845a106c0f52e2a7618f1e8cd20347b
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-05 12:17:33 +0300

    base

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet
 
-# Create your views here.
+from . import models, serializers
+
+
+class CustomUserViewSet(UserViewSet):
+    '''
+    Кастомное представление для пользователей.
+    '''
+
+    queryset = models.CustomUser.objects.all()
+    serializer_class = serializers.CustomUserSerializer
