75b4b3dea8048e11d341447191c9078cac1d6f9b
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-07 13:14:33 +0300

    add model for sub

--- backend/users/models.py
+++ backend/users/models.py
@@ -36,3 +36,37 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Sub(models.Model):
+    '''
+    Модель для подписчиков, реализованная через связь many-to-many
+    между моделями пользователей.
+    '''
+
+    user = models.ForeignKey(
+        AbstractUser,
+        on_delete=models.CASCADE,
+        related_name='subs',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        AbstractUser,
+        on_delete=models.CASCADE,
+        related_name='subs',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+        ordering = ('-id',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'author',),
+                name='unique_sub',
+            )
+        ]
+
+        def __str__(self):
+            return f'Пользователь {self.user.username} подписан на {self.author.username}'
