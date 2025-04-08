f0298e9e77d08346c96559ad407896ae7d04d9be
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-08 17:07:53 +0300

    update methods

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,8 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
+from api import serializers as srls
+
 
 User = get_user_model()
 
@@ -70,3 +72,27 @@ class SubSerializer(CustomUserSerializer):
             'first_name',
             'last_name',
         )
+
+        def get_is_subscribed(self, obj):
+            user = self.context['request'].user
+            return (
+                user.is_authenticated
+                and user.subscriber.filter(author=obj.author).exists()
+            )
+        
+        def get_recipes(self, obj):
+            request = self.context.get('request')
+            recipes_limit = request.query_params.get('recipes_limit')
+            if recipes_limit is not None:
+                try:
+                    recipes_limit = int(recipes_limit)
+                except ValueError:
+                    recipes_limit = None
+            recipes_limit_value = recipes_limit or settings.DEFAULT_PAGE_SIZE
+            recipes = obj.author.recipes.all()[:recipes_limit_value]
+
+            return srls.RecipeListSerializer(
+                recipes,
+                many=True,
+                context=self.context
+            ).data
