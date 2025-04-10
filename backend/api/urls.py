709fb320eb690faecd363cf24afc7d57250754e0
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-10 13:21:26 +0300

    update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,9 +7,21 @@ app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
 
-router_v1.register('recipes', views.RecipeViewSet, basename='recipes')
-router_v1.register('tags', views.TagViewSet, basename='tags')
-router_v1.register('ingredients', views.IngredientViewSet, basename='ingredients')
+router_v1.register(
+    'recipes', 
+    views.RecipeViewSet, 
+    basename='recipes'
+)
+router_v1.register(
+    'tags', 
+    views.TagViewSet, 
+    basename='tags'
+)
+router_v1.register(
+    'ingredients', 
+    views.IngredientViewSet, 
+    basename='ingredients'
+)
 
 urlpatterns = [
     path('', include(router_v1.urls))
