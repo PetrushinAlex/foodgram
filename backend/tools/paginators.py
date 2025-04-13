f20848a647e6ee275a880293e5c818260dde89f6
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-13 10:59:33 +0300

    correct

--- backend/tools/paginators.py
+++ backend/tools/paginators.py
@@ -1,11 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
 
+from . import constants as cnst
+
 
 class CustomPaginator(PageNumberPagination):
     '''
-    Кастомный пагинатор, устанавливающий по 6 объектов на страницу
-    и позволяющий переопределить это количество через параметр limit.
+    Кастомный пагинатор, устанавливающий по определенное в константе 
+    количество объектов на страницу и позволяющий переопределить 
+    это количество через параметр limit.
     '''
 
-    page_size = 6
+    page_size = cnst.DEFAULT_PAGE_SIZE
     page_size_query_param = 'limit'
