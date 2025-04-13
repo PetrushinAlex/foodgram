704447e685af50b3cede172bef7db6e9823024d7
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-13 10:57:35 +0300

    add rest framework settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
+from tools import constants as cnst
+
 
 load_dotenv()
 
@@ -71,6 +73,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': cnst.DEFAULT_PAGE_SIZE,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 
