d1815c18131647e53ef0edc46e2350b6897fdab9
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-04-17 16:13:40 +0300

    recipeviewset update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
 
 from . import serializers
 from food import models
-from tools import filters, paginators
+from tools import filters, paginators, permissions
 
 
 class RecipeViewSet(viewsets.GenericViewSet):
@@ -21,7 +23,24 @@ class RecipeViewSet(viewsets.GenericViewSet):
         '''
         Метод для добавления рецепта в модель.
         '''
-        pass
+        if model.objects.filter(user=user, recipe__id=pk).exists():
+            return Response(
+                {"Ошибка": "Рецепт уже был добавлен"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        recipe = get_object_or_404(
+            models.Recipe, 
+            id=pk,
+        )
+        model.objects.create(
+            user=user, 
+            recipe=recipe,
+        )
+        serializer = serializers.RecipeCreateUpdateSerializer(recipe)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED,
+        )
 
     def delete_from(self, model, user, pk):
         '''
@@ -29,8 +48,28 @@ class RecipeViewSet(viewsets.GenericViewSet):
         '''
         pass
 
-    def get_permission(self):
-        pass
+    def get_permissions(self):
+        '''
+        Переопределяет пермишен для создания, апдейта 
+        и удаления рецепта.
+        '''
+        if self.action is 'create':
+            return (IsAuthenticated(),)
+        if self.action in ('destroy', 'update',):
+            return (permissions.IsAuthorOrAdmin(),)
+        return super().get_permissions()
+    
+    def get_serializer_class(self):
+        '''
+        Переопределяет метод сериализации, разделяя ее
+        на "безопасные" методы и остальные.
+        '''
+        serializer_class = (
+            serializers.RecipeListSerializer
+            if self.request.method in SAFE_METHODS
+            else serializers.RecipeCreateUpdateSerializer
+        )
+        return serializer_class
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
