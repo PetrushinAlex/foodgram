8e9552deffbee0cf8987180afb10983b43e6b5a3
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-06-30 01:10:04 +0300

    allowed hosts

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
