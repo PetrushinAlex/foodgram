f496df80ff0d88b3a6dd17578aa8e59dc6cff437
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-06-29 21:28:18 +0300

    urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from food.views import redirect_recipe
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls", namespace="api")),
+    path('api/', include('api.urls', namespace='api')),
+    path('recipes/<int:pk>/', redirect_recipe, name='redirect_recipe'),
 ]
