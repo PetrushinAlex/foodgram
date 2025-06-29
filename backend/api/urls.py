314da398f103424b08ec27f70971ba5b2f588fe8
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-06-29 17:30:18 +0300

    url djoser

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -31,6 +31,6 @@ router_v1.register(
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('auth/', include('djoser.urls.authtoken')),
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.authtoken')),
 ]
-
