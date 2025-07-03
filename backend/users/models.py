0931e4d3fd211e4789cce88fc53d64eb3d510e58
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-07-03 09:30:53 +0300

    models-users 1 iteration

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,56 +1,49 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import RegexValidator
 
-from tools import constants
+from foodgram import constants
 
 
-class CustomUser(AbstractUser):
-    '''
+class ExtendedUser(AbstractUser):
+    """
     Модель пользователя на основе импортируемой абстрактной модели.
     Переопределены поля юзернейма, электронной почты, имени и фамилии.
     На уровне базы данных задана сортировка по id.
-    '''
-
-    username = models.CharField(
-        verbose_name='имя пользователя',
-        max_length=constants.MAX_LENGHT_NAME,
-        unique=True,
-        validators=[RegexValidator(r"^[\w.@+-]+$")],
-    )
+    """
+    
     email = models.EmailField(
         max_length=constants.MAX_LENGHT_NAME,
-        verbose_name='Электронная почта',
+        verbose_name="Электронная почта",
         unique=True,
     )
     first_name = models.CharField(
         max_length=constants.MAX_LENGHT_NAME,
-        verbose_name='Имя',
+        verbose_name="Имя",
     )
     last_name = models.CharField(
         max_length=constants.MAX_LENGHT_NAME,
-        verbose_name='Фамилия',
+        verbose_name="Фамилия",
     )
     avatar = models.ImageField(
-        upload_to='avatars/',
-        default='avatars/default_avatar.png',
+        upload_to="avatars/",
+        default="avatars/default_avatar.png",
         null=True,
     )
 
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
-    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "password"]
+    USERNAME_FIELD = "email"
 
     class Meta:
-        ordering = ['id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        ordering = ["id"]
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return self.username
 
 
 class Sub(models.Model):
-    '''
+    """
     Модель для подписчиков, реализованная через связь many-to-many
     между моделями пользователей.
     Описаны поля:
@@ -59,33 +52,36 @@ class Sub(models.Model):
     user - подписчик на автора (аналогично с автором через модель юзера).
     На уровне базы данных прописана уникальность связи
     полей автор-подписчик.
-    '''
+    """
 
     user = models.ForeignKey(
-        CustomUser,
+        ExtendedUser,
         on_delete=models.CASCADE,
-        related_name='subscriptions',
-        verbose_name='кто подписался',
+        related_name="subscriptions",
+        verbose_name="кто подписался",
     )
     author = models.ForeignKey(
-        CustomUser,
+        ExtendedUser,
         on_delete=models.CASCADE,
-        related_name='users_subscribers',
-        verbose_name='на кого подписались',
+        related_name="users_subscribers",
+        verbose_name="на кого подписались",
     )
 
     class Meta:
-        verbose_name = 'Подписчик'
-        verbose_name_plural = 'Подписчики'
+        verbose_name = "Подписчик"
+        verbose_name_plural = "Подписчики"
         constraints = [
             models.UniqueConstraint(
-                fields=('user', 'author',),
-                name='unique_sub',
+                fields=(
+                    "user",
+                    "author",
+                ),
+                name="unique_sub",
             )
         ]
 
         def __str__(self):
             return (
-                f'Пользователь {self.user.username} '
-                f'подписан на {self.author.username}'
+                f"Пользователь {self.user.username} "
+                f"подписан на {self.author.username}"
             )
