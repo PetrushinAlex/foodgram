84021ef69cf5b4e0c6aacef4fc5ecf794f362c09
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-07-07 00:39:44 +0300

    deploy

--- backend/api/views.py
+++ backend/api/views.py
@@ -316,7 +316,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 row_cells[0].text = str(idx)
                 row_cells[1].text = ing["ingredient__name"]
                 row_cells[2].text = (
-                    f"{ing['total_amount']} " 
+                    f"{ing['total_amount']} "
                     f"{ing['ingredient__measurement_unit']}"
                 )
         else:
