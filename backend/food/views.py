add33ccef301a884df648567327e71fa7076be4f
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-07-08 15:24:47 +0300

    deploy

--- backend/food/views.py
+++ backend/food/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import get_object_or_404, redirect
 
 def redirect_recipe(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    return redirect(f'recipes/{recipe.pk}/')
+    return redirect(f"recipes/{recipe.pk}/")
