6ab3a398e89498335801985146941f65a2d71d65
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-07-09 10:11:10 +0300

    def subscribe

--- backend/api/views.py
+++ backend/api/views.py
@@ -107,7 +107,7 @@ class UserViewSet(DjoserUserViewSet):
 
         serializer = myserializers.SubscriptionCreateSerializer(
             data={"user": user.id, "author": author.id},
-            context={"request": request, "view": self},
+            context={"request": request},
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
